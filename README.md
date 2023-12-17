7042d2fdc429a74286ba578ea6036627391cb18a
BiggestChris <stylianouchristopher8@gmail.com>
2023-12-17 13:59:14 +0000

    Finished javascript
    Pulling field names and inputs into database

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ACE-Flexible-Digital-Form-V1
  
+A lot of code for this submission was derived from my earlier work expanding out the Leaven Pizza digital menu app, specifically linking it to Firebase for real-time data submission and collection.
--- index.js
+++ index.js
@@ -17,8 +17,29 @@ const field3 = document.getElementById("field-3");
 const input3 = document.getElementById("input-3");
 const field4 = document.getElementById("field-4");
 const input4 = document.getElementById("input-4");
-const uploadBtn = document.getElementById("upload-btn");
+const uploadBtn = document.getElementById("upload-btn"); // Look at removing this eventually
 
 // Come back later and put above into an array
 // Find out how to read strings based on numbers so this is pulled in for any number of fields
 // Likely need to store number of fields as a variable and loop that in, may need a function to count number of fields
+
+uploadBtn.addEventListener("click", function() {
+    let field1Value = field1.value;
+    let input1Value = input1.value;
+    let field2Value = field2.value;
+    let input2Value = input2.value;
+    let field3Value = field3.value;
+    let input3Value = input3.value;
+    let field4Value = field4.value;
+    let input4Value = input4.value;
+
+    // Again, look at array for above and looping to cover all
+    
+    push(databaseInfo, {
+        field1Value: input1Value,
+        field2Value: input2Value,
+        field3Value: input3Value,
+        field4Value: input4Value
+    });
+
+})
